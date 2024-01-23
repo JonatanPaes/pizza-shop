9185ffbb874d82848553ab1b8e0651752c9002e4
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-01-22 21:23:23 -0300

    feat: Atualizando HTTP state

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -26,7 +26,7 @@ export function AccountMenu() {
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
-      queryKey: ['mandaged-restaurant'],
+      queryKey: ['managed-restaurant'],
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
     })
