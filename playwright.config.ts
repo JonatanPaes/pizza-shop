cd36603d5389ecca81f24d35277bafd0936233bc
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-12 13:55:44 -0300

    test: Teste E2E: Sign In

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test',
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   use: {
-    baseURL: 'http://localhost:50789',
+    baseURL: 'http://localhost:50790',
   },
   webServer: {
     command: 'npm run dev:test',
-    url: 'http://localhost:50789',
+    url: 'http://localhost:50790',
     reuseExistingServer: !process.env.CI,
   },
 
